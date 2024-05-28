7131dd969e17dde595a696bbc42f9e216e4c338d
Abhijithkrishna2001 <abhijithkrishna644@gmail.com>
2024-05-28 20:28:50 +0530

    First Commit
    Signed-off-by: Abhijithkrishna2001 <abhijithkrishna644@gmail.com>

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById("projectsBtn").addEventListener("click", function() {
-    alert(" ALL PROJECTS   404 Not Found");
+   alert(" ALL PROJECTS   404 Not Found");
   });
 
 
   document.getElementById("galleryBtn").addEventListener("click", function() {
-    alert("VIEW GALLERY   404 Not Found");
+   alert("VIEW GALLERY   404 Not Found");
   });
